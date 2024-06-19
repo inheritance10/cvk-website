adf3bd2dd1a0c9f441f864ba532bad298ab15b57
Ali cebeci <103434459+inheritance10@users.noreply.github.com>
2024-06-19 03:11:52 +0300

    mui and bootstrap installed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,30 @@
+import {Button, Typography} from "@mui/material";
+import Head from "next/head";
 import Image from "next/image";
+import {Container} from "@mui/material";
 
 export default function Home() {
   return (
-    <main className='flex'>
       <div>
-        CVK SOFT
+          <Head>
+              <title>CVK SOFT</title>
+              <meta name="description" content="CVK SOFT Anasayfa"/>
+              <link rel="icon" href="/favicon.ico"/>
+          </Head>
+
+          <main>
+              <Container maxWidth="md">
+                  <Typography variant="h3" component="h1" gutterBottom>
+                      Hoş Geldiniz, CVK SOFT!
+                  </Typography>
+                  <Typography variant="body1" gutterBottom>
+                      CVK SOFT, size özel çözümler sunar.
+                  </Typography>
+                  <Button variant="contained" color="primary">
+                      Daha Fazlası
+                  </Button>
+              </Container>
+          </main>
       </div>
-    </main>
   );
 }
